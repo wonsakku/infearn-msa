c15039763296cd1c068be4018fe9d987743775b1
김원식TPD <wonsik.kim@timf.co.kr>
2025-02-01 10:35:12 +0900

    EDA 구현 - 내부영역 도메인헥사곤 (대여서비스의 도메인이벤트 정의)

--- rentalMS/src/main/java/com/msa/rental/domain/model/RentalCard.java
+++ rentalMS/src/main/java/com/msa/rental/domain/model/RentalCard.java
@@ -1,5 +1,8 @@
 package com.msa.rental.domain.model;
 
+import com.msa.rental.domain.model.event.ItemRented;
+import com.msa.rental.domain.model.event.ItemReturned;
+import com.msa.rental.domain.model.event.OverdueCleared;
 import com.msa.rental.domain.model.vo.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -68,6 +71,18 @@ public class RentalCard {
         rentalCard.setRentStatus(RentStatus.RENT_AVAILABLE);
         rentalCard.setLateFee(LateFee.createLateFee());
         return rentalCard;
+    }
+
+    public static ItemRented createItemRentedEvent(IDName idName, Item item, long point){
+        return new ItemRented(idName, item, point);
+    }
+
+    public static ItemReturned createItemReturnEvent(IDName idName, Item item, long point){
+        return new ItemReturned(idName, item, point);
+    }
+
+    public static OverdueCleared createOverdueClearedEvent(IDName idName, long point){
+        return new OverdueCleared(idName, point);
     }
 
     // 대여처리
